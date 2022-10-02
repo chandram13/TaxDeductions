3dff4fcfe2a11493e446cb8c9766567ed8fd54ca
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-02 09:00:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,34 @@
 class Tax Deductions:
 
     def medDental(self, individual,partner,child1,child2):
-        individual = input("Did you visit the hospital or get dental treatment at all this year?")
+        print("Did you visit the hospital or get dental treatment at all this year?")
         print(individual)
         individual_yes = input("Please type how much you spent in total.")
-        partner = input("Did you visit the hospital or get dental treatment at all this year?")
+        print("Did you visit the hospital or get dental treatment at all this year?")
         print(partner)
-        partner_yes = input("Please type how much you spent in total.")
-        child1 = input("Did your first child visit the hospital or get dental treatment this year?")
+        print("Please type how much you spent in total.")
+        print("Did your first child visit the hospital or get dental treatment this year for first child?")
         print(child1)
-        child1_yes = input("Please type how much you spent in total.")
-        child2 = input("Did your second child visit the hospital or get dental treatment this year?")
+        print("Please type how much you spent in total.")
+        print("Did your second child visit the hospital or get dental treatment this year?")
         print(child2)
-        child2_yes = input("Please type how much you spent in total.")
+        print("Please type how much you spent in total for either children.")
         family_cost = "The total family spent on medical and hospital expenses is :" + str(individual_yes) + str(partner_yes) + str(child1_yes) + str(child2_yes) + "./n"
         print(family_cost)
 
     def investmentExpenses(self,individual,partner):
-        individual = input("Did you invest in a company or more than one company this year?")
-        individual_yes = input("Please enter how much money you have invested in.")
-        partner = input("Did you invest in a company or more than one company this year?")
-        partner_yes = input("Please enter how much money you have invested in.")
+        print("Did you invest in a company or more than one company this year?")
+        print("Please enter how much money you have invested in.")
+        print("Did your partner invest in a company or more than one company this year?")
+        print("Please enter how much money you have invested in partner.")
         print("You and your partner have decided to invest: " + str(individual_yes) + str(partner_yes) + "dollars./n")
 
-    def mortgageExpenses(self,individual,partner):
+    def mortgageExpenses(self,individual,partner, mortage_interest,mortage_insurance):
         print("As a couple, the both of you have to pay the following taxes: property, mortgage interest, and insurance.")
         individual_property = 5,000
         couple_property = 10,000
-        mortgage_interest = input("Enter the time span of your home loan being either: 15, 20, or 30 years.")
-        mortgage_insurance = input("Enter how much your home insurance is which ranges from: $1,500-$3,000.")
+        print("Enter the time span of your home loan being either: 15, 20, or 30 years for mortgage interest.")
+        print("Enter how much your home insurance is which ranges from: $1,500-$3,000 for mortgage insurance.")
         indivProp("I have spent" + str(individual_property) + "in property taxes./n")
         print(indivProp)
         coupProp("We have spent" + str(couple_property) + "in property taxes./n")
@@ -46,13 +46,13 @@ class Tax Deductions:
         int vocationalSchool = 16,500
         int publicBachelor = 31,750
         int childSchool = 13,641
-        eduIndividual = input("Are you currently attending vocational school or college?")
+        print("Are you currently attending vocational school or college?")
         print(eduIndividual)
-        eduPartner = input("Are you currently attending adult school or some college?")
+        print("Are you currently attending adult school or some college?")
         print(eduPartner)
-        eduChild1 = input("Are you currently in the K-12 system or attending some college?")
+        print("Are you currently in the K-12 system or attending some college?")
         print(eduChild1)
-        eduChild2 = input("Are you currently in the K-12 system or attending some college?")
+        print("Are you currently in the K-12 system or attending some college?")
         print(eduChild2)
 
         # if both children are attending k-12
@@ -69,9 +69,9 @@ class Tax Deductions:
     def charityExpenses(self,individualDon,partnerDon):
         print("Have either you or your partner make any donations this year, you qualify for a tax deduction.")
         print("If either of you have not donated, then there is no qualification for a tax deduction.")
-        individualDon = input("How much have you donated this year?")
+        print("How much have you donated this year?")
         print(individualDon)
-        partnerDon = input("How much have you donated this year?")
+        print("How much have you donated this year?")
         print(partnerDon)
         total_donated = individualDon + partnerDon
         print("Your total contribution equals to" + str(total_donated) + "dollars.")
